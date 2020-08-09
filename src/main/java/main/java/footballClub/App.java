0d3f19713b624e29b164d9589f2d41ecb8e24512
KrzysztofDolanski <k.dolanski@wp.pl>
2020-08-09 14:28:59 +0200

    Creating Football Club App DEMO.
    #Prerequisities
    This project uses following technologies:
    
    JDK 11,
    Hibernate
    MySQL Connector
    
    You should have access to a mysql database. Access to database is controlled via configuration file in resources.
    
    #What is to be done?
    Following functionalities should be improved:
    -You can add and list trainers - you can add and list trainers
    -Team team - adding a team (group)
    -Adding players - adding players, and listing players and adding to the team
    -Correct the fitness test and test listing. Fitness test assigned a gesture under a nickname.
    -Connection to database.

---
+++ src/main/java/main/java/footballClub/App.java
@@ -0,0 +1,7 @@
+package main.java.footballClub;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/main/java/footballClub/peoples/Trainer.java
@@ -0,0 +1,7 @@
+package main.java.footballClub.peoples;
+
+public class Trainer {
+
+    String surmane;
+    String name;
+}
